a9fa4935c712090458428f7033f176aa61a111b6
nielcapp1 <nielcapp1@student.arteveldehs.be>
2018-02-04 18:37:33 +0100

    update

--- scripts/index.js
+++ scripts/index.js
@@ -97,20 +97,16 @@ AFRAME.registerComponent('href', {
   }
 });
 
-(function () {
-  // switch to stereoscopic mode directly on page load, this needs to be after the a-scene loads.
-  var scene = document.querySelector('a-scene');
-  if (scene.hasLoaded) {
-      scene.enterVR();
-      scene.style.opacity= "1";
-  } else {
-      scene.addEventListener('loaded', function () {
-          setTimeout(function () {
-              scene.enterVR();
-              setTimeout(function () {
-                scene.style.opacity= "1";
-              }, 1000);
-          }, 500);
-      });
-  };
-})();
+AFRAME.registerComponent('auto-init-vr', {
+    init: function () {
+
+        var scene = this;
+
+        scene.el.addEventListener('loaded', function () {
+            setTimeout(function(){
+                console.log('Automatically entering VR...');
+                scene.el.sceneEl.enterVR();
+            },1000);
+        });
+    }
+});
